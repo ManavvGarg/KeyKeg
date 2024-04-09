739b4978e1752fd8fddc9971e8b84a3f580bf57d
Manav Garg <58691392+ManavvGarg@users.noreply.github.com>
2024-04-10 02:02:45 +0530

    Backend RandomForestCLF completed.

--- model/readme.md
+++ model/readme.md
@@ -1,1 +1,3 @@
-hi
+# Running Instructions
+
+Make sure you have scikit-learn=1.2.2 installed. As the model was trained using this specific version, make sure to run this server with the same scikit-learn version.
---
+++ model/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from warnings import filterwarnings
+#vectorizer = TfidfVectorizer(analyzer="char")
+
+app = Flask(__name__)
+filterwarnings()
+# Load the trained model
+model = pickle.load(open('./keykeg_rf.pkl', 'rb'))
+vectorizer = pickle.load(open('./vectorizer.pkl', 'rb'))
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # Get data from request parameters
+    password = request.args.get('password')
+    pass_array = np.array([password])
+    
+    # 151 dimension
+    sample_matrix = vectorizer.transform(pass_array) 
+    
+    # +2 dimension
+    length_pass = len(password)
+    length_normalised_lowercase = len([char for char in password if char.islower()])/len(password)
+    
+    # 151 + 2 
+    new_matrix2 = np.append(sample_matrix.toarray() , (length_pass , length_normalised_lowercase)).reshape(1,155)
+
+    # Make predictions using the loaded model
+    prediction = model.predict(new_matrix2)
+
+    if prediction == 0 :
+        return jsonify({'prediction': "Your Password is Weak"})
+    elif prediction == 1 :
+        return jsonify({'prediction': "Your Password is Normal"})
+    else:
+        return jsonify({'prediction': "Your password is Strong"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
